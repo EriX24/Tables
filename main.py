2719f577b27068051cd3e2b14e42ffb488498dca
EriX <digitaleric24@gmail.com>
2024-03-03 12:22:12 +1100

    Initial commit

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ from table.table import Table
 new_table = Table(columns=["Heading 1", "Heading 2", "Heading 3"])
 
 new_table.add_row(["1"], ["2"], ["3"], ["4"], ["5", "a", "jk"])
-# new_table.add_row(row=["2"])
-# new_table.add_row(row=["3"])
-# new_table.add_row(row=["4"])
-# new_table.add_row(row=["5"])
 
 new_table.edit_cell(cell_row=5, cell_column=7, new_value=None)
 

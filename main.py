f50a7ac85f5c956fa29af04b75edf1cbfa7a0d19
EriX <digitaleric24@gmail.com>
2024-03-04 17:06:41 +1100

    Added comments to main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from table.table import Table
 
+# make a new table
 new_table = Table(columns=["Heading 1", "Heading 2", "Heading 3"])
 
+# Add new row(s)
 new_table.add_row(["1"], ["2"], ["3"], ["4"], ["5", "a", "jk"])
 
+# Edit a cell
 new_table.edit_cell(cell_row=5, cell_column=7, new_value=None)
 
+# Print the table
 new_table.print_table()
